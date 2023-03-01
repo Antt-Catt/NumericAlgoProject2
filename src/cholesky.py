a0aa437b3d9aff868aac9c3e5c7e3f00cf80b2a3
Benoît Chénard <bchenard@enseirb-matmeca.fr>
2023-03-01 15:58:41 +0100

    chloesky incomplete

--- src/cholesky.py
+++ src/cholesky.py
@@ -1,5 +1,6 @@
 from math import*
 import numpy as np
+import random as rd
 
 def cholesky_exp(A):
     n,_=np.shape(A)
@@ -18,6 +19,36 @@ def cholesky_exp(A):
             i += 1
     return T
 
+def sym_matrices_gen(n,x):
+    S=np.zeros((n,n))
+    while (x>0):
+        i=rd.randint(n)
+        j=rd.randint(n)
+        if (S[i][j] == 0):
+            S[i][j]=rd.randint(n)
+            S[j][i]=S[i][j]
+            x -= 1
+    return S
+
+def cholesky_incomp(A):
+    n,_=np.shape(A)
+    T=np.zeros((n,n))
+    for j in range(n):
+        if (A[j][j] != 0):
+            sum1=0
+            for k in range (1,j):
+                sum1 += (T[j][k]**2)
+            T[j][j]=sqrt(A[j][j]-sum1)
+        i=0
+        while (j>=i):
+            if (T[j][i] != 0):
+                sum2=0  
+                for k in range (1,i):
+                    sum2 += T[i][k]*T[j][k]
+                T[j][i]= (A[i][j]-sum2)/(T[i][i])
+                i += 1
+    return T
+
 A=np.zeros((4,4))
 for i in range (4):
     A[i][0]=1
@@ -32,5 +63,6 @@ A[2][3]=14
 print("Matrice de test :\n",A,"\n")
 print("Résultat théorique :\n",np.linalg.cholesky(A),"\n")
 print("Résultation pour implémentation :\n",cholesky_exp(A))
+print("Résultation pour implémentation incomplète:\n",cholesky_exp(A))
 
 
