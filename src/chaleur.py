5564d7a8b0f5d5fed29869314f56354eb0c80bcd
Antton Cattarin <antton.cattarin@enseirb-matmeca.fr>
2023-03-06 11:26:15 +0100

    test chaleur + gradient

--- src/chaleur.py
+++ src/chaleur.py
@@ -1,13 +1,11 @@
 import numpy as np
+import random
+from scipy import linalg
+import matplotlib.pyplot as plt
+import gradient as grd
 
 def make_A(n):
     A = np.zeros((n**2, n**2), int)
-    # for j in range(n):
-    #     for i in range(j * n, (j + 1) * n):
-    #         A[i][i] = -4
-    #         #if i != ((j * n) - 1):
-    #         #    A[i + 1][i] = 1
-    #         #    A[i][i + 1] = 1
     for i in range(n**2):
         A[i][i] = -4
         if i % n != (n - 1):
@@ -18,4 +16,22 @@ def make_A(n):
             A[i][i + n] = 1
     return A
 
-print(make_A(4))
+
+def make_F(n):
+    return 100*np.random.rand(n**2)
+
+n = 4
+
+F = make_F(n)
+print(F)
+#plt.imshow(F, cmap='jet')
+#plt.colorbar()
+#plt.show()
+
+result1 = linalg.solve(make_A(n), F)
+result2 = grd.conjgrad(make_A(n), F, np.full(n**2, 1))
+print(result1)
+print(result2)
+# plt.imshow(result2, cmap='jet')
+# plt.colorbar()
+# plt.show()
